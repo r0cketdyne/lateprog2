e321159dce274ff791c682a041605be5effd77cc
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-15 17:15:37 -0500

    ughhh

---
+++ shitprogjetlag.py
@@ -0,0 +1,40 @@
+usr_type = input("mf what is your blood type. if you don't know check and put the data in")
+print(usr_type)
+#Problem Statement
+#A specialty donation allows you to give more of what patients need in just one appointment. #During
+#your donation, one part of your blood is collected, while the rest is returned to your body. #This process
+#is called apheresis (AY-fur-EE-sis).
+
+#In this decision problem, you are to determine the best types of donation for a particular #blood type
+#based on the following table. You must also check if any invalid blood types are entered at the #user
+#prompt.
+
+#If your blood type is O+ or O-
+#Double Red Cells
+#Whole Blood
+
+#If your blood type is A+
+#Plasma
+#Platelets
+
+#If your blood type is A-
+#Double Red Cells
+#Platelets
+
+#If your blood type is B+
+#Plasma
+#Platelets
+
+#If your blood type is B-
+#Double Red Cells
+#Platelets
+
+#If your blood type is AB+ or AB-
+#Plasma
+#Platelets
+
+#pushing this insanely late. jet lag. travel. ugh.
+#anyone that is using this code to model their own stuff
+#I am back home now and will be pushing more frequently.
+#will write from scratch. starting now. will push in less than a few hours
+#lfg
